182ea7762b8233d30a5638eb2dece607521a7a7b
polish1987 <polish1987@gmail.com>
2017-02-28 23:19:03 +0100

    Change to 4chan.api

--- 4chancrawler.py
+++ 4chancrawler.py
@@ -1,106 +1,90 @@
 #!/usr/bin/python
-
-import urllib.error
-import urllib.request
-import os.path
+import json
+import os
 import random
-import re
-import sys
-from bs4 import BeautifulSoup
+import socket
+import urllib.request
 
-__author__ = "polish"
+import requests
+import time
+
+import sys
+from requests import HTTPError
+
+POSSIBLE_ERRORS = (HTTPError,
+                   socket.error,
+                   requests.exceptions.InvalidSchema,
+                   requests.exceptions.ConnectionError)
 
 ROOT_URL = "http://boards.4chan.org"
-IMG_URL = "http://i.4cdn.org"
-ROOT_DIR = "{REPLACE TO YOUR PATH}"
+API_URL = "http://api.4chan.org"
+IMAGES_URL = "http://i.4cdn.org"
+
+ROOT_DIR = "YOUR_ROOT_DIR"
+
+
 USERAGENTS = [
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
     "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201"
 ]
 
-def ensure_dir(f):
-    f += "/"
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
 
-
-# get contents from url #
-def randomUserAgent():
+def random_user_agent():
     return random.choice(USERAGENTS)
 
-USERAGENT = randomUserAgent()
+USER_AGENT = random_user_agent()
 
 
 def get_content(url):
     try:
-        req = urllib.request.Request(url, headers={'User-Agent': USERAGENT})
-        content = urllib.request.urlopen(req).read()
-        return content
-    except urllib.error.HTTPError as e:
-        print("<--404-->", e)
-        return False
+        req = requests.get(url, headers={'User-Agent': USER_AGENT}, verify=False, timeout=20)
+        if req.status_code == 200:
+            return req.content
+    except POSSIBLE_ERRORS as error:
+        print(url)
+        print("This url causes the following error:", error)
+        return ""
+    except requests.exceptions.Timeout:
+        return "timeout"
 
+def get_image_url(thread, filename, extension):
+    return IMAGES_URL + "/" + thread + "/" + filename  + extension
 
-# save file from url #
+def get_thread_url(thread, thread_no):
+    return "/".join((API_URL, thread, "thread",  str(thread_no) + ".json"))
+
+# simple directory structure maker from url #
+def generatedir(filename, filetype):
+    fullpath = ''
+    fullpath += filetype + "/"
+    d = os.path.dirname(ROOT_DIR + fullpath)
+    if not os.path.exists(d):
+        os.makedirs(d)
+    return fullpath + filename + filetype
+
 def save_file(url, filepath):
     try:
         urllib.request.urlretrieve(url, filepath)
     except Exception as e:
         print(e)
 
-
-# simple directory structure maker from url #
-def dirsfromurl(url):
-    url = url.replace("http://", "")
-    url = url.replace("https://", "")
-    struct = url.split('/')
-    filename = struct[-1]
-    filetype = filename.split('.')[1]
-    subdir = filename.split('.')[0][0:4]
-
-    if (re.match(r"^[A-Za-z ]*$", filename) == None):
-        struct.pop()
-    fullpath = ''
-    for pathname in struct:
-        fullpath += pathname + "/"
-    fullpath += filetype + "/"
-    fullpath += subdir + "/"
-    d = os.path.dirname(ROOT_DIR + fullpath)
-    if not os.path.exists(d):
-        os.makedirs(d)
-    return fullpath + filename
+def get_thread_elements(thread):
+    current_url = "/".join((API_URL, thread, "catalog.json"))
+    content = json.loads(get_content(current_url))
+    for pages in content:
+        for threads in pages['threads']:
+            content = get_content(get_thread_url(thread, threads["no"]))
+            if content is not None:
+                thread_content = json.loads(content)
+                for post in thread_content['posts']:
+                    if "tim" in post:
+                        image_url = get_image_url(thread, str(post["tim"]), post["ext"])
+                        save_file(
+                            image_url,
+                            ROOT_DIR + generatedir(str(post["tim"]), post["ext"])
+                        )
+                        print(image_url)
+            sys.stdout.write("\n")
 
 
-def ready(thread, numofpages):
-    ensure_dir(ROOT_DIR)
-    ensure_dir(ROOT_DIR + 'i.4cdn.org')
-    ensure_dir(ROOT_DIR + 'i.4cdn.org/' + thread + '/')
-
-    i = 1
-    while i <= numofpages:
-        str_i = str(i) if i > 1 else ""
-        current_url = "/".join((ROOT_URL, thread, str_i))
-        root_url = "/".join((ROOT_URL, thread, ""))
-        # print(current_url)
-        content = get_content(current_url)
-        soupContent = BeautifulSoup(content, 'html.parser')
-        links = soupContent.findAll('a', {"class": "replylink"})
-        links = list(set(links))
-        for a in links:
-            thread_url = root_url + a['href']
-            cont = get_content(thread_url)
-            if cont != False:
-                threadContent = BeautifulSoup(cont, 'html.parser')
-                images = threadContent.findAll('a', {"class": "fileThumb"})
-                # print thread_url
-                images = list(set(images))
-                for img in images:
-                    image_source = "http:" + img['href']
-                    img_path = ROOT_DIR + dirsfromurl(image_source)
-                    save_file(image_source, img_path)
-                    print("Image: " + image_source + " saved: " + img_path)
-        i += 1
-
-
-ready(sys.argv[1], 10)
+get_thread_elements(sys.argv[1])
